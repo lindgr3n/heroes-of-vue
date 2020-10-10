f8f0776882a0c04cd5449259fa4615cecc4cc8f3
Johan Lindgren <johan@lindgr3n.com>
2020-10-10 21:17:48 +0200

    feat(chapter 2): chapter 2

--- heroes-of-vue-2/src/App.vue
+++ heroes-of-vue-2/src/App.vue
@@ -1,12 +1,22 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <router-view />
+    <router-view /> -->
+    <hero-detail />
   </div>
 </template>
+
+<script>
+import HeroDetail from "@/components/HeroDetail";
+export default {
+  components: {
+    HeroDetail
+  }
+};
+</script>
 
 <style>
 #app {
